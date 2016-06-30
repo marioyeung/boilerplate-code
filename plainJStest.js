61a7865056d019a2e9a964af1fb95b62fda9b221
Mario Yeung <maryeungth@gmail.com>
2016-06-30 00:11:51 -0700

    Create plainJStest.js

---
+++ plainJStest.js
@@ -0,0 +1,46 @@
+// require npm module
+var expect = require('chai').expect;
+var sinon = require('sinon');
+
+describe('JavaScript Mocha Boilerplate Test', function() {
+  it('boolean variable', function () {
+    expect(var).to.be.true; // or
+    expect(var).to.be.false;
+  });
+
+  it('immutable variable', function () {
+    expect(var).to.equal(val); // or 
+    expect(var).to.not.equal(val);
+  });
+  
+  it('mutable object', function () {
+    expect(var).to.eql(arr); // or
+    expect(var).to.not.eql(arr);
+  });
+  
+  it('array', function () {
+    expect(arr).to.include(item); // or
+    expect(arr).to.not.include(item);
+  });
+  
+  it('', function () {
+    expect(var).to.exist; // or
+    expect(var).to.not.exist;
+  });
+  
+  it('test data type', function () {
+    expect(var).to.be.an('undefined'); // or
+    expect(var).to.be.a('function'); // or
+    expect(arr).to.be.an.instanceof(Array); // or
+    expect(func).to.be.an.instanceof(Function); // or many more ...
+  });
+  
+  it('function invocation', function () {
+    var callback = sinon.spy();
+    var closureSpy = closure(callback);
+    closureSpy(); // simulate function call
+    
+    expect(callback).to.have.been.calledOnce; // or
+    expect(callback).to.have.been.calledTwice; // or many more ...
+  });
+})
