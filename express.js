a8f5eda9ccce63e68970f83ff013ac8563c1f028
marioyeung <maryeungth@gmail.com>
2016-07-10 15:55:27 -0700

    Update express.js boilerplate code format

--- express.js
+++ express.js
@@ -5,7 +5,7 @@ var app = express();
 
 
 // middleware
-app.use('/static', express.static(__dirname + "/../public/"));
+app.use('/static', express.static(__dirname + "/public/"));
 app.use(bodyParser.json()); // optional 1: use body parser
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -14,18 +14,18 @@ app.use(function (req, res, next) {
 
 
 // set up endpoints
-app,get('url', function (req, res) {
+app,get('<url>', function (req, res) {
   
 });
 
-app.post('url', function (req, res) {
+app.post('<url>', function (req, res) {
   
 });
 
 // and many more endpoints ...
 
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || <port-number>;
 app.listen(PORT, function(){
   console.log('Listen to the port', PORT);
 });
