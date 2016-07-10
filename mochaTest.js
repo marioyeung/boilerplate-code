a59d974b799c54d1c72b91eeccc607024728194d
marioyeung <maryeungth@gmail.com>
2016-07-10 16:23:28 -0700

    Update mochaTest.js boilerplate code format

--- mochaTest.js
+++ mochaTest.js
@@ -3,39 +3,39 @@ var expect = require('chai').expect;
 var sinon = require('sinon');
 
 describe('JavaScript Mocha Boilerplate Test', function() {
-  it('boolean variable', function () {
-    expect(var).to.be.true; // or
-    expect(var).to.be.false;
+  it('tests a boolean variable', function () {
+    expect(<var>).to.be.true; // or
+    expect(<var>).to.be.false;
   });
 
-  it('immutable variable', function () {
-    expect(var).to.equal(val); // or 
-    expect(var).to.not.equal(val);
+  it('tests an immutable variable', function () {
+    expect(<var>).to.equal(val); // or 
+    expect(<var>).to.not.equal(val);
   });
   
-  it('mutable object', function () {
-    expect(var).to.eql(arr); // or
-    expect(var).to.not.eql(arr);
+  it('tests a mutable object', function () {
+    expect(<var>).to.eql(<array>); // or
+    expect(<var>).to.not.eql(<array>);
   });
   
-  it('array', function () {
-    expect(arr).to.include(item); // or
-    expect(arr).to.not.include(item);
+  it('tests an array', function () {
+    expect(<array>).to.include(<item>); // or
+    expect(<array>).to.not.include(<item>);
   });
   
-  it('', function () {
-    expect(var).to.exist; // or
-    expect(var).to.not.exist;
+  it('tests an existence of a variable', function () {
+    expect(<var>).to.exist; // or
+    expect(<var>).to.not.exist;
   });
   
-  it('test data type', function () {
-    expect(var).to.be.an('undefined'); // or
-    expect(var).to.be.a('function'); // or
-    expect(arr).to.be.an.instanceof(Array); // or
-    expect(func).to.be.an.instanceof(Function); // or many more ...
+  it('tests the data type of a variable', function () {
+    expect(<var>).to.be.an('undefined'); // or
+    expect(<var>).to.be.a('function'); // or
+    expect(<array>).to.be.an.instanceof(Array); // or
+    expect(<function>).to.be.an.instanceof(Function); // or many more ...
   });
   
-  it('function invocation', function () {
+  it('tests the numbers of function invocation', function () {
     var callback = sinon.spy();
     var closureSpy = closure(callback);
     closureSpy(); // simulate function call
