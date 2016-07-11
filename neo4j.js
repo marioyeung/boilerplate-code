9b7d1889209920b04a51ba70d2435d4ec7cb2535
marioyeung <maryeungth@gmail.com>
2016-07-10 17:39:22 -0700

    Update neo4j format

--- neo4j.js
+++ neo4j.js
@@ -2,13 +2,13 @@
 var neo4j = require('neo4j-driver').v1;
 
 // start neo4j database
-var driver = neo4j.driver("bolt://localhost", neo4j.auth.basic("neo4j", "neo4j"));
+var driver = neo4j.driver('<bolt host>', neo4j.auth.basic('<username>', '<password>'));
 var session = driver.session();
 
 // query the neo4j database
 session
   .run(cypher)
-  .then( function( result ) {
+  .then( function(result) {
     // TODO: do with result fetched from neo4j database
     // close neo4j database
     session.close();
